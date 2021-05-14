7e4b57bd784c930d9023d9eb6d454b9a4500b9d3
marcoBrighterAI <marco@brighter.ai>
2021-05-14 08:54:42 +0200

    fix: wrong resource group in terraform backend

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    resource_group_name  = "${var.resource_group}"
+    resource_group_name  = "tstate"
     storage_account_name = "tstate17096"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
